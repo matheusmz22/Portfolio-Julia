4a1cdfa0889a90a98be79e697f0d7c5918c4db45
Matheus <matheusmzucchi@gmail.com>
2026-02-05 21:55:32 -0800

    Servant Page

--- src/pages/costume/Servant.jsx
+++ src/pages/costume/Servant.jsx
@@ -1,5 +1,31 @@
+import hero from "../../assets/servant/hero.jpg";
+import img1 from "../../assets/servant/img1.jpg";
+import img2 from "../../assets/servant/img2.jpg";
+import img3 from "../../assets/servant/img3.jpg";
+import img4 from "../../assets/servant/img4.jpg";
+import img5 from "../../assets/servant/img5.jpg";
+import img6 from "../../assets/servant/img6.jpg";
+import img7 from "../../assets/servant/img7.jpg";
+import img8 from "../../assets/servant/img8.jpg";
+import CostumeGallery from "../../Components/Costume Wardrobe/CostumeGallery";
+
+const imgs = [img1, img2, img3, img4, img5, img6, img7, img8];
+
+// The Servant of Two Masters
+//Dresser
+//Directed by Geno Car
 function Servant() {
-  return <section id="servant">servant</section>;
+  return (
+    <section id="servant">
+      <CostumeGallery
+        title="The Servant of Two Masters"
+        role="Dresser"
+        directed="Directed by Geno Car"
+        heroImage={hero}
+        images={imgs}
+      />
+    </section>
+  );
 }
 
 export default Servant;

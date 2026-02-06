660d9fe21a9999823585579a28531afcaa0b3bbc
Matheus <matheusmzucchi@gmail.com>
2026-02-05 22:30:21 -0800

    ScrollToTop Component to fix scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,24 @@ import Uniretown from "./pages/costume/Uniretown";
 import IntimateApparel from "./pages/costume/IntimateApparel";
 import Servant from "./pages/costume/Servant";
 import Bridge from "./pages/costume/Bridge";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      {/* HOME */}
-      <Route path="/" element={<Layout />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        {/* HOME */}
+        <Route path="/" element={<Layout />} />
 
-      {/* COSTUME PAGES */}
-      <Route path="/costume/twelfth-night" element={<TwelfthNight />} />
-      <Route path="/costume/urinetown" element={<Uniretown />} />
-      <Route path="/costume/intimate-apparel" element={<IntimateApparel />} />
-      <Route path="/costume/servant" element={<Servant />} />
-      <Route path="/costume/bridge" element={<Bridge />} />
-    </Routes>
+        {/* COSTUME PAGES */}
+        <Route path="/costume/twelfth-night" element={<TwelfthNight />} />
+        <Route path="/costume/urinetown" element={<Uniretown />} />
+        <Route path="/costume/intimate-apparel" element={<IntimateApparel />} />
+        <Route path="/costume/servant" element={<Servant />} />
+        <Route path="/costume/bridge" element={<Bridge />} />
+      </Routes>
+    </>
   );
 }
 

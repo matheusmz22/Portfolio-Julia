615ba543d1513b888828a859db9be76c4ad25c27
Matheus <matheusmzucchi@gmail.com>
2026-02-05 22:36:14 -0800

    Intimate Apparel Page

--- src/pages/costume/IntimateApparel.jsx
+++ src/pages/costume/IntimateApparel.jsx
@@ -1,9 +1,20 @@
+import CostumeGallery from "../../Components/Costume Wardrobe/CostumeGallery";
+
+import img1 from "../../assets/Intimate/img1.jpg";
+import img2 from "../../assets/Intimate/img2.jpg";
+import img3 from "../../assets/Intimate/img3.jpg";
+import img4 from "../../assets/Intimate/img4.jpg";
+import img5 from "../../assets/Intimate/img5.jpg";
+import img6 from "../../assets/Intimate/img6.jpg";
+
+const imgs = [img1, img2, img3, img4, img5, img6];
+
 // Intimate Apparel
 // Sketches
 function IntimateApparel() {
   return (
     <section id="intimate-apparel">
-      <div>alou</div>
+      <CostumeGallery title="Intimate Apparel" role="Sketches" images={imgs} />
     </section>
   );
 }

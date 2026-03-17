a96deb80cd020e5d32cfbb9cc1bdfc3f9906b22b
Matheus <matheusmzucchi@gmail.com>
2026-03-17 15:14:00 -0700

    Fixed file name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Routes, Route} from "react-router-dom";
 
 import Layout from "./Layout";
 import TwelfthNight from "./pages/costume/TwelfthNight";
-import Uniretown from "./pages/costume/Uniretown";
+import Urinetown from "./pages/costume/Urinetown";
 import IntimateApparel from "./pages/costume/IntimateApparel";
 import Servant from "./pages/costume/Servant";
 import Bridge from "./pages/costume/Bridge";
@@ -18,7 +18,7 @@ function App() {
 
         {/* COSTUME PAGES */}
         <Route path="/costume/twelfth-night" element={<TwelfthNight />} />
-        <Route path="/costume/urinetown" element={<Uniretown />} />
+        <Route path="/costume/urinetown" element={<Urinetown />} />
         <Route path="/costume/intimate-apparel" element={<IntimateApparel />} />
         <Route path="/costume/servant" element={<Servant />} />
         <Route path="/costume/bridge" element={<Bridge />} />

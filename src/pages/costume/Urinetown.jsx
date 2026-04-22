1942f6f9768f5056184fa940f8802dbed9641ad9
Matheus <matheusmzucchi@gmail.com>
2026-04-21 19:28:39 -0700

    Fix files name

--- src/pages/costume/Urinetown.jsx
+++ src/pages/costume/Urinetown.jsx
@@ -1,20 +1,20 @@
-import hero from "../../assets/urinetown/hero.JPG";
-import img1 from "../../assets/urinetown/img1.JPG";
-import img2 from "../../assets/urinetown/img2.JPG";
-import img3 from "../../assets/urinetown/img3.JPG";
-import img4 from "../../assets/urinetown/img4.JPG";
-import img5 from "../../assets/urinetown/img5.JPG";
-import img6 from "../../assets/urinetown/img6.JPG";
-import img7 from "../../assets/urinetown/img7.JPG";
-import img8 from "../../assets/urinetown/img8.JPG";
-import img9 from "../../assets/urinetown/img9.JPG";
-import img10 from "../../assets/urinetown/img10.JPG";
-import img11 from "../../assets/urinetown/img11.JPG";
-import img12 from "../../assets/urinetown/img12.JPG";
-import img13 from "../../assets/urinetown/img13.JPG";
-import img14 from "../../assets/urinetown/img14.JPG";
-import img15 from "../../assets/urinetown/img15.JPG";
-import img16 from "../../assets/urinetown/img16.JPG";
+import hero from "../../assets/urinetown/hero.jpg";
+import img1 from "../../assets/urinetown/img1.jpg";
+import img2 from "../../assets/urinetown/img2.jpg";
+import img3 from "../../assets/urinetown/img3.jpg";
+import img4 from "../../assets/urinetown/img4.jpg";
+import img5 from "../../assets/urinetown/img5.jpg";
+import img6 from "../../assets/urinetown/img6.jpg";
+import img7 from "../../assets/urinetown/img7.jpg";
+import img8 from "../../assets/urinetown/img8.jpg";
+import img9 from "../../assets/urinetown/img9.jpg";
+import img10 from "../../assets/urinetown/img10.jpg";
+import img11 from "../../assets/urinetown/img11.jpg";
+import img12 from "../../assets/urinetown/img12.jpg";
+import img13 from "../../assets/urinetown/img13.jpg";
+import img14 from "../../assets/urinetown/img14.jpg";
+import img15 from "../../assets/urinetown/img15.jpg";
+import img16 from "../../assets/urinetown/img16.jpg";
 
 import CostumeGallery from "../../Components/Costume Wardrobe/CostumeGallery";
 
